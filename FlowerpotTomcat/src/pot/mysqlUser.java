67f2e071a813202f289e0e779b3e6fee7019c554
lvsijian8 <499365864@qq.com>
2017-03-10 13:51:34 +0800

    这是一次完美的同步!!!!第一次!!!完美的忽略规则

---
+++ FlowerpotTomcat/src/pot/mysqlUser.java
@@ -0,0 +1,52 @@
+package pot;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ResourceBundle;
+
+public class mysqlUser {
+	public static String driver="com.mysql.jdbc.Driver";
+	public static String url="jdbc:mysql://localhost:3306/flowerpot";
+	public static String user="root";
+	public static String password="";
+	public static void User(){
+			ResourceBundle rB = ResourceBundle.getBundle("1");
+			url  = rB.getString("url");
+			user  = rB.getString("user");
+			password  = rB.getString("password");
+	}
+
+	public static boolean checkConnection(Connection conn) {
+		try {
+			Statement pingStatement = null;
+			try {
+				pingStatement = conn.createStatement();
+				pingStatement.executeQuery("SELECT 1").close();//测试是否超时
+			} finally {
+				if (pingStatement != null)
+					pingStatement.close();
+			}
+			return false;
+		} catch (Exception e) {
+			return true;
+		}
+	}
+	public static void over(ResultSet rs,Statement stat,Connection con){
+		try {
+			if (rs != null)
+				rs.close();
+			if (stat != null)
+				stat.close();
+			if (con != null)
+				con.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ FlowerpotTomcat/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: desol
+  Date: 2017/3/9
+  Time: 22:33
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$lllll
+  </body>
+</html>
