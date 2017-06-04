b3369fc06177a4b459853afc10c52a73009f4280
zepXiao <desolatexzp@gmail.com>
2017-06-04 20:51:31 +0800

    改登录注册

---
+++ MrGarden/web/js/signup.js
@@ -0,0 +1,140 @@
+$(function(){
+	
+	var pass1 = $('#password1'),
+		pass2 = $('#password2'),
+		phone = $('#phone'),
+		username = $('#username'),
+		form = $('#main form'),
+		arrow = $('#main .arrow');
+
+	// Empty the fields on load
+	$('#main .row input').val('');
+
+	// Handle form submissions
+	form.on('submit',function(e){
+		
+		// Is everything entered correctly?
+		if($('#main .row.success').length == $('#main .row').length){
+
+			alert("Success!");
+			
+		}
+		else{
+			
+			// No. Prevent form submission
+			e.preventDefault();
+			
+		}
+	});
+	
+	// Validate the username field
+	username.on('blur',function(){
+		
+		// Very simple validation
+		if (!/[^\d]{2,}/.test(username.val())){
+			username.parent().addClass('error').removeClass('success');
+		}
+		else{
+			username.parent().removeClass('error').addClass('success');
+		}
+		
+	});
+	
+	// Validate the phone field
+	phone.on('blur',function(){
+		
+		// Very simple validation
+		if (!/^[1]+[3,5,8]+\d{9}/.test(phone.val())){
+			phone.parent().addClass('error').removeClass('success');
+		}
+		else{
+			phone.parent().removeClass('error').addClass('success');
+		}
+		
+	});
+
+	// Use the complexify plugin on the first password field
+	pass1.complexify({minimumChars:2, strengthScaleFactor:0.7}, function(valid, complexity){
+		
+		if(valid){
+			pass2.removeAttr('disabled');
+			
+			pass1.parent()
+					.removeClass('error')
+					.addClass('success');
+		}
+		else{
+			pass2.attr('disabled','true');
+			
+			pass1.parent()
+					.removeClass('success')
+					.addClass('error');
+		}
+		
+		var calculated = (complexity/100)*268 - 134;
+		var prop = 'rotate('+(calculated)+'deg)';
+		
+		// Rotate the arrow
+		arrow.css({
+			'-moz-transform':prop,
+			'-webkit-transform':prop,
+			'-o-transform':prop,
+			'-ms-transform':prop,
+			'transform':prop
+		});
+	});
+	
+	// Validate the second password field
+	pass2.on('keydown input',function(){
+		
+		// Make sure its value equals the first's
+		if(pass2.val() == pass1.val()){
+			
+			pass2.parent()
+					.removeClass('error')
+					.addClass('success');
+		}
+		else{
+			pass2.parent()
+					.removeClass('success')
+					.addClass('error');
+		} 
+	});
+	
+});
+
+
+
+/*******************隐藏显示密码**********************/
+var demoImg = document.getElementById("demo_img");
+var demoInput = document.getElementById("password1");
+function hideShowPsw(){
+	if (demoInput.type == "password") {
+		demoInput.type = "text";
+		demo_img.src = "images/Visible.png";
+	}else {
+		demoInput.type = "password";
+		demo_img.src = "images/Invisible.png";
+	}
+}
+var demoImg2 = document.getElementById("demo_img2");
+var demoInput2 = document.getElementById("password2");
+function hideShowPsw2(){
+	if (demoInput2.type == "password") {
+		demoInput2.type = "text";
+		demo_img2.src = "images/Visible.png";
+	}else {
+		demoInput2.type = "password";
+		demo_img2.src = "images/Invisible.png";
+	}
+}
+
+/*************************叉叉*****************/
+$(function(){
+	$("#clear1").click(function() {
+        $("input[name='usernmae']").val("").focus(); // 清空并获得焦点
+    });
+    $("#clear2").click(function() {
+        $("input[name='phone']").val("").focus(); // 清空并获得焦点
+    });
+}) 
--- MrGarden/web/signup.jsp
+++ MrGarden/web/signup.jsp
@@ -1,54 +1,53 @@
 <%--
   Created by IntelliJ IDEA.
-  User: lvsijian8
-  Date: 2017/3/10
-  Time: 12:23
+  User: desol
+  Date: 2017/6/4
+  Time: 20:25
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>登录|注册</title>
-    <link rel="stylesheet" href="css/login_style.css" />
-
-
-    <script language="javascript">
-
-        window.onload = function () {
-            <%
-                out.print(request.getAttribute("error"));
-            %>
-        };
-    </script>
+    <title>register</title>
+    <link rel="stylesheet" href="css/signup_style.css" />
 </head>
 <body>
 
 <div id="main">
 
-    <h1>Login, jion us!</h1>
+    <h1>Sign up, it's FREE!</h1>
 
     <form class="" method="post" action="">
 
         <div class="row usernmae">
-            <input type="text" id="username" name="usernmae" placeholder="用户名" onkeyup="this.value=this.value.replace(' ','')" required/>
+            <input type="text" id="username" name="username" placeholder="用户名" onkeyup="this.value=this.value.replace(' ','')" required/>
             <img id="clear1" src="images/icon_delete.png" width="20px"/>
+        </div>
+
+        <div class="row phone">
+            <input type="text" id="phone" name="phone" placeholder="手机号" onkeyup="this.value=this.value.replace(' ','')" required/>
+            <img id="clear2" src="images/icon_delete.png" width="20px"/>
         </div>
 
         <div class="row pass">
             <input type="password" id="password1" name="password1" placeholder="密码" onkeyup="this.value=this.value.replace(' ','')" required/>
             <img id="demo_img" onclick="hideShowPsw()" src="images/Invisible.png" width="20px">
         </div>
-        <span>错误提示！！！！</span>
 
+        <div class="row pass">
+            <input type="password" id="password2" name="password2" placeholder="重输密码" onkeyup="this.value=this.value.replace(' ','')" disabled="disable" required/>
+            <img id="demo_img2" onclick="hideShowPsw2()" src="images/Invisible.png" width="20px">
+        </div>
+        <span>111</span>
         <!-- The rotating arrow -->
         <div class="arrowCap"></div>
         <div class="arrow"></div>
 
         <p class="meterText">Password Meter</p>
-        <br/><br/><br/>
-        <input type="submit" value="Login" />
 
-        <a href="signup.jsp" style="float:left;padding-left: 1em">没有账号？点击注册</a>
+        <input type="submit" value="Register" />
+
+        <a href="login.jsp" style="float:left;padding-left: 1em">已有账号？点击登录</a>
 
     </form>
 </div>
@@ -61,6 +60,6 @@
 <script src="js/jquery.complexify.js"></script>
 <script src="js/signup.js"></script>
 
-<script src="js/login.js"></script>
+
 </body>
 </html>
